850354be8e1fd7b80256e0749a20d83a338943f8
Xin <x@x.com>
2018-09-20 09:57:28 -0500

    updates

--- techniques/Palindrome.java
+++ techniques/Palindrome.java
@@ -4,29 +4,30 @@ public class Palindrome {
 
 	public static void main(String[] args) {
 		Palindrome t = new Palindrome();
-		System.out.println("abc "+t.isPalindrome(""));
-		System.out.println("aba "+t.isPalindromeNonRecur(""));
+		System.out.println("abc " + t.isPalindrome(""));
+		System.out.println("aba " + t.isPalindromeNonRecur(""));
 	}
-	
+
 	public boolean isPalindrome(String s) {
-		
-		return isPalindrome(s, 0, s.length()-1);
+
+		return isPalindrome(s, 0, s.length() - 1);
 	}
-	
+
 	public boolean isPalindrome(String s, int i, int j) {
-		if(i >= 0 && i <= j && j < s.length()) {
-			if(i+1 <= j-1)
-				return s.charAt(i) == s.charAt(j) && isPalindrome(s, i+1, j-1);
+		if (i >= 0 && i <= j && j < s.length()) {
+			if (i + 1 <= j - 1)
+				return s.charAt(i) == s.charAt(j) && isPalindrome(s, i + 1, j - 1);
 			else
 				return s.charAt(i) == s.charAt(j);
 		}
 		return true;
 	}
-	
+
 	public boolean isPalindromeNonRecur(String s) {
 		int i = 0, j = s.length() - 1;
-		while(i < j) {
-			if(s.charAt(i++) != s.charAt(j--)) return false;
+		while (i < j) {
+			if (s.charAt(i++) != s.charAt(j--))
+				return false;
 		}
 		return true;
 	}
